9056e1c734a63660a23565fafd22e78c9d965525
hugoiuri <hugoraiuri@gmail.com>
2018-10-21 18:50:22 -0300

    adicionando arquivos de paginas e componentes

---
+++ src/pages/ItemDetailPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ItemDetailPage extends Component {
+    render() {
+        return (
+            <div className="Container border">
+
+            </div>
+      );
+    };
+};
