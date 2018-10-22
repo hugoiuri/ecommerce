2c18c8c357fa29c44c3de0337f761c8546911a94
hugoiuri <hugoraiuri@gmail.com>
2018-10-22 11:55:04 -0300

    Implementando rotas no site

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './css/index.css';
-import ItemListPage from './pages/ItemListPage';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { ItemListPage, MyRequestsPage, LoginPage, ProfilePage, CartPage } from './pages';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<ItemListPage />, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter>
+  <Switch>
+    <Route path="/" exact={true} component={ItemListPage} />
+    <Route path="/meus-pedidos" component={MyRequestsPage} />
+    <Route path="/login" component={LoginPage} />
+    <Route path="/perfil" component={ProfilePage} />
+    <Route path="/carrinho" component={CartPage} />
+  </Switch>
+</ BrowserRouter>, document.getElementById('root'));
 
 serviceWorker.unregister();
