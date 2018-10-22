e2e5483afd087095a27a4836dabd241d6669ee0f
hugoiuri <hugoraiuri@gmail.com>
2018-10-21 22:27:53 -0300

    adicionando componente item e itemlist

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
+import '../css/item.css'
+
 export default props => {
-    return <div>Item</div>
+    return <div className="card card-item">
+        <img className="card-img-top" src="https://images.livrariasaraiva.com.br/imagemnet/imagem.aspx/?pro_id=10219664&qld=90&l=430&a=-1" alt="Card image cap" />
+        <div className="card-body">
+            <h5 className="card-title text-truncate">Notebook Samsung Expert X40, 8ºger.Intel®Core™I5 8250U,8Gb,Hd1tb, 2Gb Geforce®Mx110, W10,15",Preto</h5>
+            <p className="card-text"><del>De: R$ 3.549,00</del><br/>
+            Por: <span className="price">R$ 2.499,00</span><br/>
+            em 3x no crédito</p>
+            <a href="#" className="btn btn-primary">Comprar</a>
+        </div>
+    </div>
 };
